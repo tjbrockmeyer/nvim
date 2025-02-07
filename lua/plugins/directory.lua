7b7a9e817248b6da30652bead7ecff8721dacc17
tjbrockmeyer <tyler@teabee.dev>
2025-02-07 17:31:55 -0600

    move icons to be a dep of directory

--- lua/plugins/directory.lua
+++ lua/plugins/directory.lua
@@ -1,1 +1,13 @@
-return { "prichrd/netrw.nvim", opts = {} }
+return {
+	"prichrd/netrw.nvim",
+	opts = {},
+	dependencies = {
+		{
+			"echasnovski/mini.nvim",
+			version = false,
+			config = function()
+				require("mini.icons").setup()
+			end,
+		},
+	},
+}
